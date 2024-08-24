c9063e38d2d535ca05e76eec17e82613e8554092
Ajoyrockets <ajoypartapsingh@gmail.com>
2024-08-24 17:18:04 +0100

    hello there

--- Task_1/variable_and_types.py
+++ Task_1/variable_and_types.py
@@ -1,4 +1,18 @@
+# types of variables 
 "string"
 "floats"
+"interger"
 "boolean"
 "lists"
+
+# numbers
+
+myint = 7          # interger
+print (myint)
+
+myfloat = 7.0      #float
+print (myfloat)
+myfloat = float(7)
+print(myfloat)
+
+#strings 
