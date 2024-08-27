d93adb354aefc1de94ae4750fb0326a188b1740e
Ajoyrockets <ajoypartapsingh@gmail.com>
2024-08-27 16:02:29 +0100

    modified:   Task_1/String_formating.py modified:   task_2/Window _testing.py

---
+++ Task_1/String_formating.py
@@ -0,0 +1,17 @@
+name = "John"
+print("Hello, %s!" % name)
+
+#python uses % operators to format variables and %s and %d 
+#are special symbols
+
+name = "John"
+age = 23
+print("%s is %d years old." % (name, age))
+
+#if there are more then 2 arguements specifiers then use
+#paraentheses
+
+mylist = [1,2,3]
+print("A list: %s" % mylist)
+
+#
--- task_2/Window _testing.py
+++ task_2/Window _testing.py
@@ -2,12 +2,17 @@ import tkinter as tk
 from tkinter import ttk
 
 def button_one_press():
+    button_one.destroy()
+    button_two.destroy()
     label = tk.Label(root, text="you pressed button 1 ", font=("Helvetica", 16))
     label.pack(pady=20)
 
 def button_two_press():
+    button_one.destroy()
+    button_two.destroy()
     label = tk.Label(root, text="you pressed button 2 ", font=("Helvetica", 16))
     label.pack(pady=20)
+
 
 
 # Create the main window
