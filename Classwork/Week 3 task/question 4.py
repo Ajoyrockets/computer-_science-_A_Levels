a9f5db72fdad84853f8c6efa9deb8a188da5f6b6
Ajoyrockets <ajoypartapsingh@gmail.com>
2024-10-06 20:19:39 +0100

    modified:   Classwork/Week 3 task/question 4.py

---
+++ Classwork/Week 3 task/question 4.py
@@ -0,0 +1,7 @@
+# Write a Python program that asks the user for two integers and prints their sum.
+
+a = int(input("Input the first value"))
+b = int(input("Input the second value"))
+
+c = a + b
+print (c)
