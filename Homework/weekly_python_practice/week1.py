97c8a2501e38e0f04a4854fb1f143c8cb1357cd2
Ajoypartap Singh <ajoypartapsingh@gmail.com>
2025-05-06 22:20:48 +0100

    new file:   Homework/weekly_python_practice/week1.py

---
+++ Homework/weekly_python_practice/week1.py
@@ -0,0 +1,86 @@
+"""
+If x = 5, what is a correct syntax for printing the data type of the variable x?
+
+print(dtype(x))
+print(type(x))
+print(x.dtype())
+
+ans print(type(x))
+
+///////////////////////
+
+x = 5
+print(type(x))
+
+ans integer (int)
+
+////////////////////////
+
+x = "Hello World"
+print(type(x))
+
+ans string (str)
+
+///////////////////////
+
+x = 20.5
+print(type(x))
+
+ans float 
+
+///////////////////////
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+ans list 
+
+////////////////////////
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+ans ?? ("correct answer is tuple")
+
+////////////////////////
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+ans ?? ("dict, dictionary")
+
+////////////////////////
+
+x = True
+print(type(x))
+
+ans boolean 
+
+////////////////////////
+
+##############################################################
+
+python casting 
+
+//////////////////////
+
+What will be the result of the following code:
+print(int(35.88))
+
+35
+35.88
+36
+
+ans 35
+
+/////////////////////////
+
+print(float(35))
+
+35
+35.0
+0.35
+
+ans 35.0
+
+"""
